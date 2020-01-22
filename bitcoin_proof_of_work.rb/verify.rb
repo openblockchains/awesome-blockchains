6cb5996d288acab9aa341e0d108ee96f82ab8fc3
Gerald Bauer <gerald.bauer@gmail.com>
2020-01-22 21:26:24 +0100

    Update verify.rb

--- bitcoin_proof_of_work.rb/verify.rb
+++ bitcoin_proof_of_work.rb/verify.rb
@@ -13,9 +13,14 @@ end
 nonce = 4250
 msg   = "Hello, world!"
 
-difficulty = 2**240
-hash = sha256( "#{msg}#{nonce}" )
-num  = hash.to_i(16)
+p hash = sha256( "#{msg}#{nonce}" )
+#=> "0000c3af42fc31103f1fdc0151fa747ff87349a4714df7cc52ea464e12dcd4e9"
+
+p difficulty = 2**240
+#=> 1766847064778384329583297500742918515827483896875618958121606201292619776
+p num        = hash.to_i(16)
+#=> 1350565582647790482127632554504241516291697500941742491868079705537959145
 
 p num < difficulty
 #=> true
+
